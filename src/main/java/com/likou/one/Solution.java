e98fa3a637d6cb5117ce3adf97f643d00c38b48f
linqianxu <linqianxu@engintime.com>
2020-12-17 09:41:58 +0800

    2020/12/17-714. 买卖股票的最佳时机含手续费

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -6,22 +6,25 @@ import javafx.util.Pair;
 import java.util.*;
 
 class Solution {
+    /**
+     * 由于转移过程之与前一个相关, 因此也可以只采用两个变量保存状态
+     */
     public static int maxProfit(int[] prices, int fee) {
-        if (prices.length == 1 || prices.length == 0) {
-            return 0;
+        int length = prices.length;
+        int [][]dp = new int[length][2];
+        //dp[i][0]用于保存第i天没有股票的最大利润
+        //dp[i][1]用于保存第i天持有股票的最大利润
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0] - fee;
+        for (int i = 1; i < length; i++) {
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i] - fee);
         }
-        int total = 0;
-        for (int i = 0; i < prices.length-1; i++) {
-            int temp = prices[i+1] - prices[i] - fee;
-            if (temp > 0) {
-                total += temp;
-            }
-        }
-        return total;
+        return dp[length-1][0];
     }
 
     public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
+        System.out.println(maxProfit(new int[]{1, 3, 7, 5, 10, 3}, 3));
     }
 }
 
