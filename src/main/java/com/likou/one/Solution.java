f8e10f30869cd49dd5248aca97e1c1a27ae67c03
linqianxu <linqianxu@engintime.com>
2020-12-18 11:08:31 +0800

    2020/12/17-714.买卖股票的最佳时机含手续费

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -7,24 +7,62 @@ import java.util.*;
 
 class Solution {
     /**
-     * 由于转移过程之与前一个相关, 因此也可以只采用两个变量保存状态
+     * 统计字符串s的字符频率
+     * 再统计字符串t的字符频率
+     * 若某个字符出现的频率不一致, 则返回即可
      */
-    public static int maxProfit(int[] prices, int fee) {
-        int length = prices.length;
-        int [][]dp = new int[length][2];
-        //dp[i][0]用于保存第i天没有股票的最大利润
-        //dp[i][1]用于保存第i天持有股票的最大利润
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0] - fee;
-        for (int i = 1; i < length; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i] - fee);
+    public static char findTheDifference1(String s, String t) {
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+        int []frequent = new int[26];
+        for (int i = 0; i < charS.length; i++) {
+            frequent[charS[i]-'a']++;
         }
-        return dp[length-1][0];
+        for (int i = 0; i < charT.length; i++) {
+            frequent[charT[i]-'a']--;
+            if (frequent[charT[i]-'a'] < 0) {
+                return charT[i];
+            }
+        }
+        return ' ';
+    }
+
+    /**
+     * 通过(^异或)位运算可把出现次数为偶次的字符去掉
+     * A^A=0
+     */
+    public static char findTheDifference2(String s, String t) {
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+        int result = 0;
+        for (char aChar : charS) {
+            result ^= aChar;
+        }
+        for (char c : charT) {
+            result ^= c;
+        }
+        return (char) result;
+    }
+
+    /**
+     * 通过两个字符串ASCII码值总和相减
+     * 差值即为被添加的字符
+     */
+    public static char findTheDifference(String s, String t) {
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+        int as = 0, at = 0;
+        for (char c : charS) {
+            as += c;
+        }
+        for (char c : charT) {
+            at += c;
+        }
+        return (char) (at - as);
     }
 
     public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
+        System.out.println(findTheDifference("abcd", "abcde"));
     }
 }
 
