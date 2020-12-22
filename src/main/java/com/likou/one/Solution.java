515aacafeac684092547d073885ec1e9baf6a1ea
linqianxu <linqianxu@engintime.com>
2020-12-22 08:45:43 +0800

    746. 使用最小花费爬楼梯

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -6,47 +6,21 @@ import javafx.util.Pair;
 import java.util.*;
 
 class Solution {
-    /**
-     * 通过查找规律, 发现matrix[i][j]经过翻转会变到matrix[j][n-i-i]
-     * 通过这一规律使用原地翻转的方式
-     * 通过画图易知对于长度为偶数的矩阵我们只需要枚举(n/2)*(n/2)个位置
-     * 长度为奇数的矩阵我们需要枚举((n+1)/2)*((n+1)/2)个位置
-     */
-    public static void rotate1(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < n / 2; i++) {
-            for (int j = 0; j < (n + 1) / 2; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[n-j-1][i];
-                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
-                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
-                matrix[j][n-i-1] = temp;
-            }
-        }
-    }
 
     /**
-     * 旋转90度
-     * 可分解为如下过程
-     * 1.水平翻转
-     * 2.主对角线翻转
+     * 由于到达第i层可从i-1层或i-2层开始
+     * 定义数组dp[n+1]
+     * dp[0],dp[1]表示第一第二层
      */
-    public static void rotate(int[][] matrix) {
-        int n = matrix.length;
-        //水平翻转
-        for (int i = 0; i < n / 2; i++) {
-            int[] temp = matrix[i];
-            matrix[i] = matrix[n-i-1];
-            matrix[n-i-1] = temp;
+    public static int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i < n+1; i++) {
+            dp[i] = Math.min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
         }
-        //对角线翻转
-        for (int i = 0; i < n-1; i++) {
-            for (int j = i+1; j < n; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
+        return dp[n];
     }
 
 
