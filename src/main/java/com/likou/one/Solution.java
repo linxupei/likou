403a19051168ebe0c77ce2a50830e28b63a984ca
linqianxu <linqianxu@engintime.com>
2021-01-15 22:51:23 +0800

    947. 移除最多的同行或同列石头

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -6,26 +6,57 @@ import javafx.util.Pair;
 import java.util.*;
 
 class Solution {
-    /**
-     * 首先统计每一个字符出现的个数
-     * 然后遍历字符串判断是否有出现次数为1的字符,
-     * 有则返回下标即可
-     */
-    public int firstUniqChar(String s) {
-        char[] chars = s.toCharArray();
-        int length = chars.length;
-        int []frequent = new int[26];
-        for (int i = 0; i < length; i++) {
-            frequent[chars[i]-'a']++;
+
+    public int removeStones(int[][] stones) {
+        UnionFind unionFind = new UnionFind();
+        for (int[] stone :
+                stones) {
+            unionFind.union(~stone[0], stone[1]);
+            unionFind.union(stone[0]-10001, stone[1]);
+            unionFind.union(stone[0]+10001, stone[1]);
         }
-        for (int i = 0; i < length; i++) {
-            if (frequent[chars[i]-'a'] == 1) {
-                return i;
-            }
-        }
-        return -1;
+        return stones.length - unionFind.getCount();
     }
 
+    private class UnionFind {
+        private Map<Integer, Integer> parent;
+
+        private int count;
+
+        public UnionFind() {
+            this.parent = new HashMap<>();
+            this.count = 0;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public int find(int x) {
+            if (!parent.containsKey(x)) {
+                parent.put(x, x);
+                //并查集集中新加入一个结点, 结点的父亲是它自己, 所以联通分量的总数+1
+                count++;
+            }
+
+            if (x != parent.get(x)) {
+                parent.put(x, find(parent.get(x)));
+            }
+
+            return parent.get(x);
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return;
+            }
+
+            parent.put(rootX, rootY);
+            count--;
+        }
+    }
 
     public static void main(String[] args) {
 
