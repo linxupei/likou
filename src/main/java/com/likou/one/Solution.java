d4efda7ac037df3d7fa3e80c50c664138e8ff366
linqianxu <linqianxu@engintime.com>
2021-02-06 13:39:47 +0800

    605. 种花问题

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -6,40 +6,88 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    /**
-     * 分成3种情况讨论, 设下标l, r对应位置为1
-     * 1.(l, r)间全为0, 可供种植的位置有l-r-2, 最多种植(l-r-2)/2
-     * 2.l左边全为0, 可供种植的位置为l-1, 最多种植(l-1)/2
-     * 3.r右边全为0, 可供种植的位置为length-r-1, 最多种植(length-r-1)/2
-     */
-    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int pre = -1;
-        int length = flowerbed.length;
-        int total = 0;
-        for (int i = 0; i < length; i++) {
-            if (flowerbed[i] == 1) {
-                if (pre < 0) {
-                    //第二种情况
-                    total += (i / 2);
-                } else {
-                    //第一种情况
-                    total += ((i - pre - 2) / 2);
-                }
-                pre = i;
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] prefixMax = new int[n];
+        int[] suffixMax = new int[n];
+        for (int i = 0; i < n; ++i) {
+            if (i % k == 0) {
+                prefixMax[i] = nums[i];
+            }
+            else {
+                prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
             }
         }
-        if (pre < 0) {
-            //第一种情况
-            total += ((length + 1) / 2);
-        } else {
-            //第三种情况
-            total += ((length - pre - 1) / 2);
+        for (int i = n - 1; i >= 0; --i) {
+            if (i == n - 1 || (i + 1) % k == 0) {
+                suffixMax[i] = nums[i];
+            } else {
+                suffixMax[i] = Math.max(suffixMax[i + 1], nums[i]);
+            }
         }
-        return total >= n;
+
+        int[] ans = new int[n - k + 1];
+        for (int i = 0; i <= n - k; ++i) {
+            ans[i] = Math.max(suffixMax[i], prefixMax[i + k - 1]);
+        }
+        return ans;
+    }
+
+    /**
+     * 单调递减队列
+     */
+    public static int[] maxSlidingWindow2(int[] nums, int k) {
+        int length = nums.length;
+        int[] maxNums = new int[length - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offer(i);
+        }
+        maxNums[0] = nums[deque.peekFirst()];
+        for (int i = k; i < length; i++) {
+            //若当前值比队列末尾值大, 移除队列末尾值
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            //将队列头中下标不符合滑动窗口范围的移除
+            while (!deque.isEmpty() && deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            deque.offer(i);
+            maxNums[i-k+1] = nums[deque.peekFirst()];
+        }
+        return maxNums;
+    }
+
+    /**
+     * 通过优先队列, 大根堆
+     * 将当前下标以及下标对应的值放入大根堆中
+     * 先比较值, 再比较下标
+     */
+    public static int[] maxSlidingWindow_(int[] nums, int k) {
+        int length = nums.length;
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((o1, o2) -> o1[0] != o2[0] ? o2[0]-o1[0] : o2[1]-o1[1]);
+        int[] maxNums = new int[length - k + 1];
+        for (int i = 0; i < k; i++) {
+            priorityQueue.offer(new int[]{nums[i], i});
+        }
+        maxNums[0] = priorityQueue.peek()[0];
+        for (int i = k; i < length; i++) {
+            priorityQueue.offer(new int[]{nums[i], i});
+            //当根节点的下标不在滑动窗口范围内的时候, 移除
+            while (priorityQueue.peek()[1] <= i - k) {
+                priorityQueue.poll();
+            }
+            maxNums[i-k+1] = priorityQueue.peek()[0];
+        }
+        return maxNums;
     }
 
     public static void main(String[] args) {
-        System.out.println(canPlaceFlowers(new int[]{1,0,0,0,1}, 1));
+        System.out.println(maxSlidingWindow(new int[]{1,3,1,2,0,5}, 3));
     }
 }
 
