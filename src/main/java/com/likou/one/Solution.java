5f0c4bbcb6cd578c94283191146c08244d8d3f45
linqianxu <linqianxu@engintime.com>
2021-02-06 10:17:09 +0800

    1423. 可获得的最大点数

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -7,34 +7,35 @@ import java.util.stream.Collectors;
 
 class Solution {
     /**
-     * 首先把所有字符之间的差值找出来, 依次作为串
-     * 然后通过逐渐扩大的滑动窗口, 把符合条件的子串找出来
-     * 窗口扩大: total > maxCost(即窗口花费小于最大花费)
+     * 创建一个数组(长度为2k, 这是两端顶点能到达的最大值)用于保存用于窗口滑动的数组
+     * 先将cardPoints的下标k-1->0放入数组, 再将下标length->length-k放入数组
+     * 最后进行窗口滑动即可, 需判断k==length?
      */
-    public static int equalSubstring(String s, String t, int maxCost) {
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
-        int length = sChars.length;
-        int[] differ = new int[length];
+    public static int maxScore(int[] cardPoints, int k) {
+        int[] points = new int[2 * k];
+        int length = cardPoints.length;
         int total = 0;
-        int left = 0, right = 0;
-        for (int i = 0; i < length; i++) {
-            differ[i] = Math.abs(tChars[i] - sChars[i]);
+        int max = 0;
+        for (int i = k - 1; i >= 0; i--) {
+            points[k - 1 - i] = cardPoints[i];
+            total += cardPoints[i];
         }
-        while (right < length) {
-            int temp = differ[right];
-            total += temp;
-            right++;
-            if (total > maxCost) {
-                total -= differ[left];
-                left++;
+        max = total;
+        for (int i = length - 1; i >= length - k; i--) {
+            points[length + k - 1 - i] = cardPoints[i];
+        }
+        if (k != length) {
+            for (int i = k; i < 2 * k; i++) {
+                total += points[i];
+                total -= points[i-k];
+                max = Math.max(max, total);
             }
         }
-        return right - left;
+        return max;
     }
 
     public static void main(String[] args) {
-        System.out.println(equalSubstring(new String("krrgw"), new String("zjxss"), 19));
+        System.out.println(maxScore(new int[]{1,2,3,4,5,6,1}, 3));
     }
 }
 
