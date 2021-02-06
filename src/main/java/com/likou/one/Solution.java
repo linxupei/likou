d872fe2c3464091759825f239773476cead80c41
linqianxu <linqianxu@engintime.com>
2021-02-06 21:16:12 +0800

    509. 斐波那契数

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -6,39 +6,25 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-
-    public static class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
-
     /**
-     * 将所有小于x的结点放入listNode1
-     * 其余放入listNode2
-     * 最后合并两个链表即可
-     * 此方法不会改变原链表
+     * 按照公式逐步求解即可
      */
-    public static ListNode partition(ListNode head, int x) {
-        ListNode head1 = new ListNode();
-        ListNode listNode1 = head1;
-        ListNode head2 = new ListNode();
-        ListNode listNode2 = head2;
-        ListNode temp = head;
-        while (temp != null) {
-            if (temp.val < x) {
-                listNode1.next = new ListNode(temp.val);
-                listNode1 = listNode1.next;
-            } else {
-                listNode2.next = new ListNode(temp.val);
-                listNode2 = listNode2.next;
-            }
-            temp = temp.next;
+    public int fib(int n) {
+        int a = 1, b = 0;
+        int index = 1;
+        if (n == 0) {
+            return b;
         }
-        listNode1.next = head2.next;
-        return head1.next;
+        if (n == 1) {
+            return a;
+        }
+        while (index < n) {
+            int temp = a;
+            a = a + b;
+            b = temp;
+            index++;
+        }
+        return a;
     }
 
     public static void main(String[] args) {
