5a1c654298c1d5facabf25f5fceddadbd59a6195
linqianxu <linqianxu@engintime.com>
2021-02-07 10:25:28 +0800

    830. 较大分组的位置

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -6,34 +6,39 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    public static boolean checkPossibility(int[] nums) {
-        int times = 0;
-        int length = nums.length;
-        for (int i = 0; i < length-1; i++) {
-            if (nums[i] > nums[i+1]) {
-                if (i == 0) {
-                    //若当前下标为0, 则使他变小一点
-                    nums[i] = nums[i+1];
-                } else if (i == length - 2) {
-                    //若当前下标为length-2, 则使最后一个位置变大一点
-                    nums[length - 1] = nums[length - 2];
-                } else {
-                    if (nums[i+1] >= nums[i-1]) {
-                        //若该情况类似...7, 1, 8..., 则使当前位置变小一点
-                        nums[i] = nums[i-1];
-                    } else {
-                        //若该情况类似...7, 8, 2..., 则下一个位置变大一点
-                        nums[i+1] = nums[i];
-                    }
+    /**
+     * 顺序遍历列表即可
+     * 需要注意最后一组是否大于2
+     */
+    public List<List<Integer>> largeGroupPositions(String s) {
+        List<List<Integer>> result = new LinkedList<>();
+        char[] chars = s.toCharArray();
+        int length = chars.length;
+        int start = 0;
+        char current = chars[0];
+        for (int i = 1; i < length; i++) {
+            if (chars[i] != current) {
+                if (i - start > 1) {
+                    List<Integer> temp = new LinkedList<>();
+                    temp.add(start);
+                    temp.add(i - 1);
+                    result.add(temp);
                 }
-                times++;
+                start = i;
+                current = chars[i];
             }
         }
-        return times <= 1;
+        if (length - start > 2) {
+            List<Integer> temp = new LinkedList<>();
+            temp.add(start);
+            temp.add(length - 1);
+            result.add(temp);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(checkPossibility(new int[]{5,7,1,8}));
+
     }
 }
 
