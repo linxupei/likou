fecb7a66859a9bd7edd5dd92fb34dad364f4ad92
linqianxu <linqianxu@engintime.com>
2021-02-07 09:52:17 +0800

    665. 非递减数列

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -6,29 +6,34 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    /**
-     * 按照公式逐步求解即可
-     */
-    public int fib(int n) {
-        int a = 1, b = 0;
-        int index = 1;
-        if (n == 0) {
-            return b;
+    public static boolean checkPossibility(int[] nums) {
+        int times = 0;
+        int length = nums.length;
+        for (int i = 0; i < length-1; i++) {
+            if (nums[i] > nums[i+1]) {
+                if (i == 0) {
+                    //若当前下标为0, 则使他变小一点
+                    nums[i] = nums[i+1];
+                } else if (i == length - 2) {
+                    //若当前下标为length-2, 则使最后一个位置变大一点
+                    nums[length - 1] = nums[length - 2];
+                } else {
+                    if (nums[i+1] >= nums[i-1]) {
+                        //若该情况类似...7, 1, 8..., 则使当前位置变小一点
+                        nums[i] = nums[i-1];
+                    } else {
+                        //若该情况类似...7, 8, 2..., 则下一个位置变大一点
+                        nums[i+1] = nums[i];
+                    }
+                }
+                times++;
+            }
         }
-        if (n == 1) {
-            return a;
-        }
-        while (index < n) {
-            int temp = a;
-            a = a + b;
-            b = temp;
-            index++;
-        }
-        return a;
+        return times <= 1;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(checkPossibility(new int[]{5,7,1,8}));
     }
 }
 
