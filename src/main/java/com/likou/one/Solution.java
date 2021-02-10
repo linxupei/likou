13b8d3a81d92a94219064692ec7788adfb98cfe9
linqianxu <linqianxu@engintime.com>
2021-02-10 20:54:59 +0800

    567. 字符串的排列

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -7,48 +7,60 @@ import java.util.stream.Collectors;
 
 class Solution {
     /**
-     * 设区间[left, right]是满足条件的子区间
-     * 那么保持right不变, left变化
-     * 一定会存在一个i<=left<=j的区间使得[left, right]是满足条件
-     * 当left再小一点(种类>K)或再大一点(种类<K)都不满足条件,
+     * frequent用于统计子字符串的每个字母出现的频率
+     * account用于统计子字符串的字母种类个数
+     * 当s2中的某个字符串使得account==0的时候说明存在答案
      */
-    public static int subarraysWithKDistinct(int[] A, int K) {
-        int length = A.length;
-        int total1 = 0, total2 = 0;
-        int result = 0;
-        int[] num1 = new int[length+1], num2 = new int[length+1];
-        int left1 = 0, left2 = 0, right = 0;
-        while (right < length) {
-            if (num1[A[right]] == 0) {
-                total1++;
-            }
-            num1[A[right]]++;
-            if (num2[A[right]] == 0) {
-                total2++;
-            }
-            num2[A[right]]++;
-            while (total1 > K) {
-                num1[A[left1]]--;
-                if (num1[A[left1]] == 0) {
-                    total1--;
-                }
-                left1++;
-            }
-            while (total2 > K-1) {
-                num2[A[left2]]--;
-                if (num2[A[left2]] == 0) {
-                    total2--;
-                }
-                left2++;
-            }
-            result += left2 - left1;
-            right++;
+    public static boolean checkInclusion(String s1, String s2) {
+        char[] chars1 = s1.toCharArray();
+        char[] chars2 = s2.toCharArray();
+        int length1 = chars1.length, length2 = chars2.length;
+        int[] frequent = new int[26];
+        int account = 0;
+        int min = Integer.MIN_VALUE;
+        if (length1 > length2) {
+            return false;
         }
-        return result;
+        //值为min说明该字母没有出现在子字符串中
+        Arrays.fill(frequent, min);
+        for (char ch : chars1) {
+            if (frequent[ch-'a'] == min) {
+                account++;
+                frequent[ch-'a'] = 0;
+            }
+            frequent[ch-'a']++;
+        }
+        for (int i = 0; i < length1; i++) {
+            frequent[chars2[i]-'a']--;
+            if (frequent[chars2[i]-'a'] == 0) {
+                account--;
+                if (account == 0) {
+                    return true;
+                }
+            }
+        }
+        for (int i = 0; i < length2 - length1; i++) {
+            if (frequent[chars2[i]-'a'] != min) {
+                if (frequent[chars2[i]-'a'] == 0) {
+                    account++;
+                }
+                frequent[chars2[i]-'a']++;
+            }
+            if (frequent[chars2[i+length1]-'a'] != min) {
+                frequent[chars2[i+length1]-'a']--;
+                if (frequent[chars2[i+length1]-'a'] == 0) {
+                    account--;
+                    if (account == 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(subarraysWithKDistinct(new int[]{1,2}, 1));
+        System.out.println(checkInclusion("abc", "ccccbbbbaaaa"));
     }
 }
 
