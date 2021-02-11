b20ee78682e4ca4e13882dc67f42025d8d918c1d
linqianxu <linqianxu@engintime.com>
2021-02-11 10:13:48 +0800

    703. 数据流中的第 K 大元素

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -7,60 +7,38 @@ import java.util.stream.Collectors;
 
 class Solution {
     /**
-     * frequent用于统计子字符串的每个字母出现的频率
-     * account用于统计子字符串的字母种类个数
-     * 当s2中的某个字符串使得account==0的时候说明存在答案
+     * 通过使用只保存k个数字的小根堆
+     * 每次只放入最大的k个数, 那么根节点就是第k大的数
      */
-    public static boolean checkInclusion(String s1, String s2) {
-        char[] chars1 = s1.toCharArray();
-        char[] chars2 = s2.toCharArray();
-        int length1 = chars1.length, length2 = chars2.length;
-        int[] frequent = new int[26];
-        int account = 0;
-        int min = Integer.MIN_VALUE;
-        if (length1 > length2) {
-            return false;
-        }
-        //值为min说明该字母没有出现在子字符串中
-        Arrays.fill(frequent, min);
-        for (char ch : chars1) {
-            if (frequent[ch-'a'] == min) {
-                account++;
-                frequent[ch-'a'] = 0;
-            }
-            frequent[ch-'a']++;
-        }
-        for (int i = 0; i < length1; i++) {
-            frequent[chars2[i]-'a']--;
-            if (frequent[chars2[i]-'a'] == 0) {
-                account--;
-                if (account == 0) {
-                    return true;
+    class KthLargest {
+        int k;
+        List<Integer> nums;
+        PriorityQueue<Integer> kNum;
+
+        public KthLargest(int k, int[] nums) {
+            this.k = k;
+            this.nums = new LinkedList<>();
+            this.kNum = new PriorityQueue<>();
+            for (int i = 0; i < nums.length; i++) {
+                this.nums.add(nums[i]);
+                kNum.offer(nums[i]);
+                if (i >= k && !kNum.isEmpty()) {
+                    kNum.poll();
                 }
             }
         }
-        for (int i = 0; i < length2 - length1; i++) {
-            if (frequent[chars2[i]-'a'] != min) {
-                if (frequent[chars2[i]-'a'] == 0) {
-                    account++;
-                }
-                frequent[chars2[i]-'a']++;
+
+        public int add(int val) {
+            kNum.add(val);
+            if (!kNum.isEmpty() && kNum.size() > k) {
+                kNum.poll();
             }
-            if (frequent[chars2[i+length1]-'a'] != min) {
-                frequent[chars2[i+length1]-'a']--;
-                if (frequent[chars2[i+length1]-'a'] == 0) {
-                    account--;
-                    if (account == 0) {
-                        return true;
-                    }
-                }
-            }
+            return kNum.size() == k ? kNum.peek() : null;
         }
-        return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(checkInclusion("abc", "ccccbbbbaaaa"));
+
     }
 }
 
