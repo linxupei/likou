54d46716f87a2fb386b831869a62d3fd915b3e85
linqianxu <linqianxu@engintime.com>
2021-02-13 09:51:48 +0800

    448. 找到所有数组中消失的数字

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -6,35 +6,28 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    //按照公式求解即可cur[j] = pre[j-1] + pre[j];
-    public static List<Integer> getRow(int rowIndex) {
+    /**
+     * 由于数组中的的数字范围在[0,n], 那么以每个数字为下标
+     * 将该下标的值加n, 那么若不存在某个数字, 那么该下标的值
+     * 就不会大于n
+     */
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> result = new LinkedList<>();
-        int[] cur = new int[rowIndex+1];
-        int[] pre = new int[rowIndex+1];
-        pre[0] = 1;
-        pre[1] = 1;
-        if (rowIndex < 2) {
-            for (int i = 0; i <= rowIndex; i++) {
-                result.add(1);
-            }
-        } else {
-            for (int i = 1; i < rowIndex; i++) {
-                cur[0] = 1;
-                for (int j = 1; j <= i; j++) {
-                    cur[j] = pre[j-1] + pre[j];
-                }
-                cur[i+1] = 1;
-                pre = Arrays.copyOf(cur, i+2);
-            }
-            for (int i = 0; i < rowIndex; i++) {
-                result.add(cur[i]);
+        int length = nums.length;
+        for (int num : nums) {
+            int x = (num - 1) % length;
+            nums[x] += length;
+        }
+        for (int i = 0; i < length; i++) {
+            if (nums[i] <= length) {
+                result.add(i+1);
             }
         }
         return result;
     }
 
     public static void main(String[] args) {
-        getRow(3);
+        System.out.println(findDisappearedNumbers(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
     }
 }
 
