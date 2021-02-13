a7eb953b624ee7d58d42ffc58eb68b9760d3db9d
linqianxu <linqianxu@engintime.com>
2021-02-13 09:19:16 +0800

    119. 杨辉三角 II

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -6,39 +6,35 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    /**
-     * 通过使用只保存k个数字的小根堆
-     * 每次只放入最大的k个数, 那么根节点就是第k大的数
-     */
-    class KthLargest {
-        int k;
-        List<Integer> nums;
-        PriorityQueue<Integer> kNum;
-
-        public KthLargest(int k, int[] nums) {
-            this.k = k;
-            this.nums = new LinkedList<>();
-            this.kNum = new PriorityQueue<>();
-            for (int i = 0; i < nums.length; i++) {
-                this.nums.add(nums[i]);
-                kNum.offer(nums[i]);
-                if (i >= k && !kNum.isEmpty()) {
-                    kNum.poll();
+    //按照公式求解即可cur[j] = pre[j-1] + pre[j];
+    public static List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new LinkedList<>();
+        int[] cur = new int[rowIndex+1];
+        int[] pre = new int[rowIndex+1];
+        pre[0] = 1;
+        pre[1] = 1;
+        if (rowIndex < 2) {
+            for (int i = 0; i <= rowIndex; i++) {
+                result.add(1);
+            }
+        } else {
+            for (int i = 1; i < rowIndex; i++) {
+                cur[0] = 1;
+                for (int j = 1; j <= i; j++) {
+                    cur[j] = pre[j-1] + pre[j];
                 }
+                cur[i+1] = 1;
+                pre = Arrays.copyOf(cur, i+2);
+            }
+            for (int i = 0; i < rowIndex; i++) {
+                result.add(cur[i]);
             }
         }
-
-        public int add(int val) {
-            kNum.add(val);
-            if (!kNum.isEmpty() && kNum.size() > k) {
-                kNum.poll();
-            }
-            return kNum.size() == k ? kNum.peek() : null;
-        }
+        return result;
     }
 
     public static void main(String[] args) {
-
+        getRow(3);
     }
 }
 
