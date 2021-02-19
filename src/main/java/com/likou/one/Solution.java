00c024b8b7cc4c9c5103edb438ee50229253151f
linqianxu <linqianxu@engintime.com>
2021-02-19 09:59:45 +0800

    995. K 连续位的最小翻转次数

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -6,30 +6,72 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    /**
-     * 行优先遍历原数组的每一个元素, 放入结果数组即可
-     */
-    public static int[][] matrixReshape(int[][] nums, int r, int c) {
-        int row = nums.length;
-        int col = nums[0].length;
-        //判断原数组元素个数是否与结果数组相同
-        if (row*col != r*c) {
-            return nums;
-        }
-        int[][] result = new int[r][c];
-        int index = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                result[index/c][index%c] = nums[i][j];
-                index++;
+    public static int minKBitFlips(int[] A, int K) {
+        int length = A.length;
+        int result = 0;
+        int[] diff = new int[length+1];
+        int revCnt = 0;
+        for (int i = 0; i < length; i++) {
+            //代表当前位置发生的翻转次数
+            revCnt += diff[i];
+            //由于数字只有0或1, 若当前数字加翻转次数为偶数
+            //则经过说明revCnt次翻转后, 当前数字为0
+            //当前位置任然需要进行一次翻转
+            if ((revCnt + A[i])%2 == 0) {
+                //最后一个小于K的区间任然有0, 无法再进行翻转
+                if (i+K > length) {
+                    return -1;
+                }
+                //翻转次数加1
+                result++;
+                //当前位置翻转次数加1
+                revCnt++;
+                //下一个区间的初始位置翻转次数减1, 即相对于前一个区间少翻转一次
+                //到达这个位置时需要减少一次翻转次数
+                diff[i+K]--;
             }
+        }
+        return result;
+    }
+
+    public static int minKBitFlips_timeout(int[] A, int K) {
+        int length = A.length;
+        int left = 0;
+        int result = 0;
+        while (left < length) {
+            if (A[left] == 0) {
+                //当剩余的元素小于K, 此时已无法翻转
+                if (left > length-K) {
+                    return -1;
+                }
+                boolean flag = false;
+                int temp = left+K-1;
+                for (int i = left; i < left+K; i++) {
+                    if (A[i] == 1) {
+                        A[i] = 0;
+                        if (!flag) {
+                            temp = i - 1;
+                            flag = true;
+                        }
+                    } else {
+                        A[i] = 1;
+                    }
+                }
+                left = temp;
+                result++;
+                //情况1: K=0, 且数组元素不全为1;
+                if (A[left] == 0) {
+                    return -1;
+                }
+            }
+            left++;
         }
         return result;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(matrixReshape(new int[][]{{1,2,4,3},{1,2,4,3}}, 4, 2));
+        System.out.println(minKBitFlips(new int[]{0,0,0,1,0,1,1,0}, 3));
     }
 }
 
