10e523160ed3f7b3b0313948e89277a1ff0c931c
linqianxu <linqianxu@engintime.com>
2021-02-20 08:21:43 +0800

    697. 数组的度

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -6,72 +6,45 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    public static int minKBitFlips(int[] A, int K) {
+    /**
+     * 滑动窗口, K的大小即子串允许存在多少个0
+     * 以right为子串右边界, left为子串左边界
+     * 当子串出现0的次数大于K时, 统计子串的长度,
+     * 并将left移动到使子串0的次数不大于K的位置
+     * right为主动移动, left为被动移动
+     */
+    public static int longestOnes(int[] A, int K) {
+        int max = 0;
+        int left = 0, right = 0;
+        int flag = 0;
         int length = A.length;
-        int result = 0;
-        int[] diff = new int[length+1];
-        int revCnt = 0;
-        for (int i = 0; i < length; i++) {
-            //代表当前位置发生的翻转次数
-            revCnt += diff[i];
-            //由于数字只有0或1, 若当前数字加翻转次数为偶数
-            //则经过说明revCnt次翻转后, 当前数字为0
-            //当前位置任然需要进行一次翻转
-            if ((revCnt + A[i])%2 == 0) {
-                //最后一个小于K的区间任然有0, 无法再进行翻转
-                if (i+K > length) {
-                    return -1;
+        while (right < length) {
+            if (A[right] == 0) {
+                K--;
+                if (K == 0 && flag <= left) {
+                    flag = right;
                 }
-                //翻转次数加1
-                result++;
-                //当前位置翻转次数加1
-                revCnt++;
-                //下一个区间的初始位置翻转次数减1, 即相对于前一个区间少翻转一次
-                //到达这个位置时需要减少一次翻转次数
-                diff[i+K]--;
-            }
-        }
-        return result;
-    }
-
-    public static int minKBitFlips_timeout(int[] A, int K) {
-        int length = A.length;
-        int left = 0;
-        int result = 0;
-        while (left < length) {
-            if (A[left] == 0) {
-                //当剩余的元素小于K, 此时已无法翻转
-                if (left > length-K) {
-                    return -1;
-                }
-                boolean flag = false;
-                int temp = left+K-1;
-                for (int i = left; i < left+K; i++) {
-                    if (A[i] == 1) {
-                        A[i] = 0;
-                        if (!flag) {
-                            temp = i - 1;
-                            flag = true;
+                if (K < 0) {
+                    max = Math.max(max, right-left);
+//                    left = flag;
+//                    flag = right;
+//                    K++;
+                    while (K < 0) {
+                        if (A[left] == 0) {
+                            K++;
                         }
-                    } else {
-                        A[i] = 1;
+                        left++;
                     }
                 }
-                left = temp;
-                result++;
-                //情况1: K=0, 且数组元素不全为1;
-                if (A[left] == 0) {
-                    return -1;
-                }
             }
-            left++;
+            right++;
         }
-        return result;
+        return Math.max(max, right-left);
     }
 
 
     public static void main(String[] args) {
-        System.out.println(minKBitFlips(new int[]{0,0,0,1,0,1,1,0}, 3));
+        System.out.println(longestOnes(new int[]{0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1}, 3));
     }
 }
 
