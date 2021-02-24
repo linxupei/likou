d8761d5888d5959b564c1d951f00c82feca7a2c8
linqianxu <linqianxu@engintime.com>
2021-02-24 12:48:08 +0800

    832. 翻转图像

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -7,37 +7,30 @@ import java.util.stream.Collectors;
 
 class Solution {
     /**
-     * 先统计老板没有抑制自己的情绪之前的顾客满意数
-     * 再通过滑动窗口找出最多顾客不满意X这个时段
-     */
-    public static int maxSatisfied(int[] customers, int[] grumpy, int X) {
-        int result = 0;
-        int max = 0;
-        int temp = 0;
-        int length = customers.length;
-        for (int i = 0; i < X; i++) {
-            if (grumpy[i] == 1) {
-                max += customers[i];
-            } else {
-                result += customers[i];
+     * 异或(^), 将两个数字化为二进制, 从高位开始比较
+     * 相同则为0, 不同则为1
+     **/
+    public static int[][] flipAndInvertImage(int[][] A) {
+        int row = A.length;
+        int col = A[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col>>1; j++) {
+                A[i][j] ^= 1;
+                A[i][col-j-1] ^= 1;
+                int temp = A[i][j];
+                A[i][j] = A[i][col-j-1];
+                A[i][col-j-1] = temp;
+            }
+            if (col % 2 != 0) {
+                A[i][col>>1] ^= 1;
             }
         }
-        temp = max;
-        for (int i = X; i < length; i++) {
-            temp += customers[i] * grumpy[i];
-            temp -= customers[i-X] * grumpy[i-X];
-            if (grumpy[i] == 0) {
-                result += customers[i];
-            }
-            max = Math.max(max, temp);
-        }
-        return result+max;
+        return A;
     }
 
-
     public static void main(String[] args) {
-        System.out.println(maxSatisfied(new int[]{1,0,1,2,1,1,7,5},
-                                        new int[]{0,1,0,1,0,1,0,1}, 3));
+        System.out.println(flipAndInvertImage(new int[][]{{1,1,0},
+                                        {1,0,1},{0,0,0}}));
     }
 }
 
