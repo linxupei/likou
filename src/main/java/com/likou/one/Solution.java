17a4a74cf761d683744ebdb6b5a26a8d2a16f6e9
linqianxu <linqianxu@engintime.com>
2021-03-02 23:33:26 +0800

    304. 二维区域和检索 - 矩阵不可变

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -22,16 +22,16 @@ class Solution {
             if (col == 0) {
                 return;
             }
-            this.matrix = new int[row+1][col+1];
+            this.matrix = new int[row + 1][col + 1];
             for (int i = 1; i <= row; i++) {
                 for (int j = 1; j <= col; j++) {
-                    this.matrix[i][j] = this.matrix[i-1][j] + this.matrix[i][j-1] + matrix[i-1][j-1] - this.matrix[i-1][j-1];
+                    this.matrix[i][j] = this.matrix[i - 1][j] + this.matrix[i][j - 1] + matrix[i - 1][j - 1] - this.matrix[i - 1][j - 1];
                 }
             }
         }
 
         public int sumRegion(int row1, int col1, int row2, int col2) {
-            return matrix[row2+1][col2+1] - matrix[row1][col2+1] - matrix[row2+1][col1] + matrix[row1][col1];
+            return matrix[row2 + 1][col2 + 1] - matrix[row1][col2 + 1] - matrix[row2 + 1][col1] + matrix[row1][col1];
         }
     }
 
