792c63e87366a27572cbdb410ccff16c190ba727
linqianxu <linqianxu@engintime.com>
2021-03-02 23:29:52 +0800

    304. 二维区域和检索 - 矩阵不可变

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -8,30 +8,41 @@ import java.util.stream.Collectors;
 class Solution {
 
     /**
-     * 前缀和
+     * 利用区域前缀和即可
      */
-    class NumArray {
-        private int[] nums;
-        private int[] sum;
+    static class NumMatrix {
+        private int[][] matrix;
 
-
-        public NumArray(int[] nums) {
-            int length = nums.length;
-            this.nums = new int[length];
-            sum = new int[length+1];
-            for (int i = 0; i < length; i++) {
-                this.nums[i] = nums[i];
-                sum[i+1] = sum[i] + nums[i];
+        public NumMatrix(int[][] matrix) {
+            int row = matrix.length;
+            if (row == 0) {
+                return;
+            }
+            int col = matrix[0].length;
+            if (col == 0) {
+                return;
+            }
+            this.matrix = new int[row+1][col+1];
+            for (int i = 1; i <= row; i++) {
+                for (int j = 1; j <= col; j++) {
+                    this.matrix[i][j] = this.matrix[i-1][j] + this.matrix[i][j-1] + matrix[i-1][j-1] - this.matrix[i-1][j-1];
+                }
             }
         }
 
-        public int sumRange(int i, int j) {
-            return sum[j] - sum[i];
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+            return matrix[row2+1][col2+1] - matrix[row1][col2+1] - matrix[row2+1][col1] + matrix[row1][col1];
         }
     }
 
     public static void main(String[] args) {
-        System.out.println();
+        NumMatrix temp = new NumMatrix(new int[][]{
+                {3, 0, 1, 4, 2},
+                {5, 6, 3, 2, 1},
+                {1, 2, 0, 1, 5},
+                {4, 1, 0, 1, 7},
+                {1, 0, 3, 0, 5}});
+        System.out.println(temp.sumRegion(2, 1, 4, 3));
     }
 }
 
