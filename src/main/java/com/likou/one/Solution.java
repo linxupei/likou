64b9236b7760897ebfa0ae9fbb5e49f0d84cb1d0
linqianxu <linqianxu@engintime.com>
2021-03-14 07:55:52 +0800

    706. 设计哈希映射

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -9,106 +9,69 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-class Solution {
+public class Solution {
 
     /**
-     * 通过一次遍历, 记录数字前的符号, 中和数字前符号以及括号外符号
-     * -(-1-(-1))
+     * 普通的哈希映射
      */
-    public static int calculate(String s) {
-        Deque<Integer> stack = new LinkedList<>();
-        int total = 0;
-        int sign = 1;
-        int index = 0;
-        int length = s.length();
-        stack.push(1);
-        while (index < length) {
-            if (s.charAt(index) == ' ') {
-                index++;
-            } else if (s.charAt(index) == '+') {
-                index++;
-                sign = stack.peek();
-            } else if (s.charAt(index) == '-') {
-                index++;
-                //改变当前符号: 如-(-1), 即对括号外符号取反
-                sign = -stack.peek();
-            } else if (s.charAt(index) == '(') {
-                index++;
-                stack.push(sign);
-            } else if (s.charAt(index) == ')') {
-                //弹出sign(), 括号前的sign
-                index++;
-                stack.pop();
-            } else {
-                int temp = 0;
-                while (index < length && Character.isDigit(s.charAt(index))) {
-                    temp = temp * 10 + (s.charAt(index) - '0');
-                    index++;
-                }
-                total += temp * sign;
+    class MyHashMap {
+        private static final int BASE = 769;
+        private LinkedList[] data;
+
+        /** Initialize your data structure here. */
+        public MyHashMap() {
+            data = new LinkedList[BASE];
+            for (int i = 0; i < BASE; i++) {
+                data[i] = new LinkedList<int []>();
             }
         }
-        return total;
-    }
 
-    /**
-     * 先处理最底层的括号, 将底层括号转换成结果, 再处理外层括号
-     */
-    public int calculate1(String s) {
-        StringBuffer stringBuffer = new StringBuffer(s);
-        Stack<Integer> stack = new Stack<Integer>();
-        int index = 0;
-
-        while (index < stringBuffer.length()) {
-            if (stringBuffer.charAt(index) == '(') {
-                stack.push(index);
-            }
-            if (stringBuffer.charAt(index) == ')') {
-                int start = stack.pop();
-                String temp = stringBuffer.substring(start + 1, index);
-                stringBuffer.delete(start, index + 1);
-                String insert = "" + dual(temp);
-                stringBuffer.insert(start, insert);
-                index = start + insert.length() - 1;
-            }
-            index++;
-        }
-        return dual(stringBuffer.toString());
-    }
-
-    /**
-     * 处理单重括号内的表达式
-     */
-    public int dual(String s) {
-        int total = 0;
-        int index = 0;
-        int flag = 1;
-        int temp = 0;
-        while (index < s.length()) {
-            char ch = s.charAt(index);
-            if (ch >= '0' && ch <= '9') {
-                temp = temp * 10 + (int) (ch - '0');
-            } else if (ch == '+') {
-                total += flag * temp;
-                temp = 0;
-                flag = 1;
-            } else if (ch == '-') {
-                total += flag * temp;
-                temp = 0;
-                flag = -1;
-                //判断是否出现'--'的情况
-                if (s.charAt(index + 1) == '-') {
-                    flag = 1;
-                    index++;
+        /** value will always be non-negative. */
+        public void put(int key, int value) {
+            int hash = hash(key);
+            Iterator iterator = data[hash].iterator();
+            for (Object o : data[hash]) {
+                int[] next = (int []) o;
+                if (next[0] == key) {
+                    data[hash].remove(next);
                 }
             }
-            index++;
+            data[hash].offerLast(new int[]{key, value});
         }
-        return total + flag * temp;
+
+        /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+        public int get(int key) {
+            int hash = hash(key);
+            Iterator iterator = data[hash].iterator();
+            for (Object o : data[hash]) {
+                int[] next = (int []) o;
+                if (next[0] == key) {
+                    return next[1];
+                }
+            }
+            return -1;
+        }
+
+        /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+        public void remove(int key) {
+            int hash = hash(key);
+            Iterator iterator = data[hash].iterator();
+            for (Object o : data[hash]) {
+                int[] next = (int []) o;
+                if (next[0] == key) {
+                    data[hash].remove(next);
+                    return;
+                }
+            }
+        }
+
+        public int hash(int key) {
+            return key % BASE;
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(calculate("1001"));
+
     }
 }
 
