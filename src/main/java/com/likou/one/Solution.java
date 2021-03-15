69c280e0788a5cde85c3673766abee5a698450ab
linqianxu <linqianxu@engintime.com>
2021-03-15 10:45:27 +0800

    54. 螺旋矩阵

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -10,68 +10,117 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Solution {
+    /**
+     * 层次遍历
+     */
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int n = rows * cols;
+        List<Integer> result = new ArrayList<>(n);
+        int left = 0, right = cols - 1, top = 0, bottom = rows - 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            for (int i = top + 1; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            if (left < right && top < bottom) {
+                for (int i = right - 1; i > left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                for (int i = bottom; i > top; i--) {
+                    result.add(matrix[i][left]);
+                }
+            }
+            left++;
+            top++;
+            right--;
+            bottom--;
+        }
+        return result;
+    }
 
     /**
-     * 平平无奇的哈希映射
+    * 模拟顺时针遍历
+    **/
+    public static List<Integer> spiralOrder1(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int n = rows * cols;
+        boolean[][] judge = new boolean[rows][cols];
+        int row = 0, col = 0;
+        List<Integer> result = new ArrayList<>(n);
+        int[][] directions = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            result.add(matrix[row][col]);
+            judge[row][col] = true;
+            //判断下一个位置是否超出边界或者已经遍历过
+            int nrow = row + directions[index][0];
+            int ncol = col + directions[index][1];
+            if (nrow < 0 || nrow >= rows || ncol < 0 || ncol >= cols || judge[nrow][ncol]) {
+                index = (index + 1) % 4;
+            }
+            row += directions[index][0];
+            col += directions[index][1];
+        }
+        return result;
+    }
+
+    /**
+     * 顺时针
      */
-    class MyHashMap {
-        private static final int BASE = 769;
-        private LinkedList[] data;
-
-        /** Initialize your data structure here. */
-        public MyHashMap() {
-            data = new LinkedList[BASE];
-            for (int i = 0; i < BASE; i++) {
-                data[i] = new LinkedList<int []>();
+    public int[][] generateMatrix1(int n) {
+        int[][] result = new int[n][n];
+        int total = n * n;
+        boolean[][] judge = new boolean[n][n];
+        int row = 0, col = 0, index = 0;
+        int[][] directions = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+        for (int i = 0; i < total; i++) {
+            result[row][col] = i + 1;
+            judge[row][col] = true;
+            //判断下一个位置是否超出边界或者已经遍历过
+            int nrow = row + directions[index][0];
+            int ncol = col + directions[index][1];
+            if (nrow < 0 || nrow >= n || ncol < 0 || ncol >= n || judge[nrow][ncol]) {
+                index = (index + 1) % 4;
             }
+            row += directions[index][0];
+            col += directions[index][1];
         }
+        return result;
+    }
 
-        /** value will always be non-negative. */
-        public void put(int key, int value) {
-            int hash = hash(key);
-            Iterator iterator = data[hash].iterator();
-            for (Object o : data[hash]) {
-                int[] next = (int []) o;
-                if (next[0] == key) {
-                    data[hash].remove(next);
-                }
+    public static int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        int left = 0, right = n - 1, top = 0, bottom = n - 1;
+        int index = 1, total = n * n;
+        while (index <= total) {
+            for (int i = left; i <= right; i++) {
+                result[top][i] = index++;
             }
-            data[hash].offerLast(new int[]{key, value});
-        }
-
-        /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-        public int get(int key) {
-            int hash = hash(key);
-            Iterator iterator = data[hash].iterator();
-            for (Object o : data[hash]) {
-                int[] next = (int []) o;
-                if (next[0] == key) {
-                    return next[1];
-                }
+            for (int i = top + 1; i <= bottom; i++) {
+                result[i][right] = index++;
             }
-            return -1;
-        }
-
-        /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-        public void remove(int key) {
-            int hash = hash(key);
-            Iterator iterator = data[hash].iterator();
-            for (Object o : data[hash]) {
-                int[] next = (int []) o;
-                if (next[0] == key) {
-                    data[hash].remove(next);
-                    return;
-                }
+            for (int i = right - 1; i >= left; i--) {
+                result[bottom][i] = index++;
             }
+            for (int i = bottom - 1; i > top; i--) {
+                result[i][left] = index++;
+            }
+            left++;
+            top++;
+            right--;
+            bottom--;
         }
-
-        public int hash(int key) {
-            return key % BASE;
-        }
+        return result;
     }
 
     public static void main(String[] args) {
-
+        generateMatrix(3);
+        //spiralOrder(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
     }
 }
 
