32f5ddb05061e10dedd1cf1773d061eb08c8f25b
linqianxu <linqianxu@engintime.com>
2021-03-20 07:35:01 +0800

    92. 反转链表 II

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -10,85 +10,78 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
 public class Solution {
     /**
-     * 先找到被翻转的第一个结点的前一个位置, 而后使用头插入法, 插入被翻转的范围结点
-     * 最后再连上没被翻转的结点
+     * 使用该方法效率极低
      */
-    public static ListNode reverseBetween(ListNode head, int left, int right) {
-        if (right - left < 1) {
-            return head;
+    public static int evalRPN1(String[] tokens) {
+        int top0, top1 = 0, top2 = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        for (String str : tokens) {
+            try {
+                top0 = Integer.parseInt(str);
+                stack.push(top0);
+            } catch (Exception e) {
+                top1 = stack.pop();
+                top2 = stack.pop();
+                if (str.equals("+")) {
+                    stack.push(top2 + top1);
+                } else if (str.equals("-")) {
+                    stack.push(top2 - top1);
+                } else if (str.equals("*")) {
+                    stack.push(top2 * top1);
+                } else if (str.equals("/")) {
+                    stack.push(top2 / top1);
+                }
+            }
         }
-        ListNode tempHead = new ListNode();
-        ListNode result = tempHead;
-        tempHead.next = head;
-        for (int i = 0; i < left - 1; i++) {
-            tempHead = tempHead.next;
-        }
-        ListNode last = tempHead.next;
-        ListNode leftHead = tempHead.next;
-        tempHead.next = null;
-        while (left <= right) {
-            ListNode temp = leftHead;
-            leftHead = leftHead.next;
-            temp.next = tempHead.next;
-            tempHead.next = temp;
-            left++;
-        }
-        last.next = leftHead;
-        return result.next;
+        return stack.pop();
     }
 
     /**
-     * 直接头结点插入法即可
+     * 正经方法
+     * 可使用数组代替
+     * 对于一个有效的波兰表达式字符串个数为奇数
+     * 数字刚好比运算符多一个
+     * 即数字(n+1)>>1, 运算符(n-1)>>1
+     * 即考虑最坏情况,字符串数组前面全部是数字, 最多需要空间(n+1)>>1
      */
-    public ListNode reverseList1(ListNode head) {
-        ListNode tempHead = new ListNode();
-        tempHead.next = null;
-        ListNode result = tempHead;
-        while (head != null) {
-            ListNode temp = head;
-            head = head.next;
-            temp.next = tempHead.next;
-            tempHead.next = temp;
+    public static int evalRPN(String[] tokens) {
+        int top1 = 0, top2 = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        for (String str : tokens) {
+            if (str.equals("+")) {
+                top1 = stack.pop();
+                top2 = stack.pop();
+                stack.push(top2 + top1);
+            } else if (str.equals("-")) {
+                top1 = stack.pop();
+                top2 = stack.pop();
+                stack.push(top2 - top1);
+            } else if (str.equals("*")) {
+                top1 = stack.pop();
+                top2 = stack.pop();
+                stack.push(top2 * top1);
+            } else if (str.equals("/")) {
+                top1 = stack.pop();
+                top2 = stack.pop();
+                stack.push(top2 / top1);
+            } else {
+                stack.push(Integer.parseInt(str));
+            }
         }
-        return result.next;
-    }
-
-    /**
-     * 假设链表为1->2->3->4->5->6->null
-     * 经过遍历一部分之后1->2-3->6<-5<-4
-     * 要使6指向3, 则3.next.next=3;
-     * 再让3.next=null即可
-     */
-    public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode result = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return result;
+        return stack.pop();
     }
 
     public static void main(String[] args) {
+        int i=10 ;
+        List list = null;
+        Set set = null;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        //hashMap.put(null, 1);
+        Hashtable<Integer, Integer> hashtable = new Hashtable<>();
+        hashtable.put(1, null);
+        System.out.println(i++==10 ? ++i : --i);
     }
 }
 
