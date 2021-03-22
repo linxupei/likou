3e84bc970dd15acf082d877cf2824ae6ab3e7501
linqianxu <linqianxu@engintime.com>
2021-03-22 09:36:07 +0800

    191. 位1的个数

--- src/main/java/com/likou/one/Solution.java
+++ src/main/java/com/likou/one/Solution.java
@@ -12,85 +12,29 @@ import java.util.stream.Collectors;
 
 public class Solution {
     /**
-     * 使用第一行第一列作为记录某一行某一列是否需要变为0
-     * 开始前要记录第一行第一列是否含有0,如果有
-     * 在最后需要将第一行或第一列变为0
+     * 普通的求二进制含有1的个数的方法
      */
-    public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean rowFlag = false, colFlag = false;
-        for (int i = 0; i < m; i++) {
-            if (matrix[0][i] == 0) {
-                rowFlag = true;
-                break;
-            }
+    public int hammingWeight1(int n) {
+        Integer.bitCount(n);
+        int count = 0;
+        while (n != 0) {
+            //通过该式可减少二进制的最低的1
+            //10110 & (10101) = 10100
+            n = (n & (n-1));
+            count++;
         }
-        for (int i = 0; i < n; i++) {
-            if (matrix[i][0] == 0) {
-                colFlag = true;
-                break;
-            }
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = matrix[0][j] = 0;
-                }
-            }
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        if (rowFlag) {
-            for (int i = 0; i < m; i++) {
-                matrix[0][i] = 0;
-            }
-        }
-        if (colFlag) {
-            for (int i = 0; i < n; i++) {
-                matrix[i][0] = 0;
-            }
-        }
+        return count;
     }
 
     /**
-     * 使用额外空间O(m +　n)用于保存某一行某一列需要变为0
+     * 汉明解法
      */
-    public void setZeroes1(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean[] row = new boolean[m];
-        boolean[] col = new boolean[n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
-                }
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (row[i] || col[j]) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-    }
-
-    public static void toZero(int row, int col, int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        for (int i = 0; i < n; i++) {
-            if (matrix[row][i] == 0) {
-                toZero(row, i, matrix);
-            }
-        }
+    public int hammingWeight(int n) {
+        n = (n & 0x55555555) + ((n >> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+        n = (n & 0x0F0F0F0F) + ((n >> 4) & 0x0F0F0F0F);
+        n = (n * 0x01010101 >> 24);
+        return n;
     }
 
 
