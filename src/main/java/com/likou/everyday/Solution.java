7a83b2ec2bcd117dbf748ba9635f75d85b4040ea
linqianxu <linqianxu@engintime.com>
2021-03-24 23:23:08 +0800

    456. 132 模式

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -1,65 +1,107 @@
 package com.likou.everyday;
 
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
-    public interface NestedInteger {
-
-        // @return true if this NestedInteger holds a single integer, rather than a nested list.
-        public boolean isInteger();
-
-        // @return the single integer that this NestedInteger holds, if it holds a single integer
-        // Return null if this NestedInteger holds a nested list
-        public Integer getInteger();
-
-        // @return the nested list that this NestedInteger holds, if it holds a nested list
-        // Return null if this NestedInteger holds a single integer
-        public List<NestedInteger> getList();
-    }
-
-    /**
-     * 注意: 不需要实现NestedInteger接口, 注意题目说明
-     * 使用深度优先遍历将所有数据存储到列表即可
-     * 使用ArrayList是因为测试数据更多使用next()函数, 获取数据时间复杂度为O(1)
-     */
-    public class NestedIterator implements Iterator<Integer> {
-        List<Integer> integerList;
-        Integer index;
-
-        public NestedIterator(List<NestedInteger> nestedList) {
-            index = 0;
-            integerList = new ArrayList<>();
-            getList(integerList, nestedList);
+    public static boolean find132pattern1(int[] nums) {
+        int[] stack = new int[3];
+        int n = nums.length;
+        int index = 0;
+        if (n < 3) {
+            return false;
         }
-
-        public void getList(List<Integer> integerList, List<NestedInteger> nestedList) {
-            for (NestedInteger nested : nestedList) {
-                if (nested.isInteger()) {
-                    integerList.add(nested.getInteger());
-                } else {
-                    getList(integerList, nested.getList());
+        for (int j = 0; j < n - 2; j++) {
+            index = 0;
+            stack[index] = nums[j];
+            for (int i = j; i < n; i++) {
+                if (nums[i] < stack[index] && index == 0) {
+                    stack[index] = nums[i];
+                    if (i > j) {
+                        j = i;
+                    }
+                }
+                if (nums[i] > stack[index]) {
+                    if (index == 0) {
+                        index++;
+                    }
+                    stack[index] = nums[i];
+                }
+                if (index == 1 && nums[i] < stack[1] && nums[i] > stack[0]) {
+                    return true;
                 }
             }
         }
+        return false;
+    }
 
-        @Override
-        public Integer next() {
-            return integerList.get(index++);
+    /**
+     * i<j<k && nums[i] < nums[k] < nums[i]
+     * 使用leftMin记录当前下标(j)的左边的最小值表示(i)
+     * 使用TreeMap记录当前下标的右边的所有值TreeMap使用红黑树实现
+     * 其中一个函数ceilingKey(value)可查找第一个比value大或等于value的值
+     */
+    public static boolean find132pattern2(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return false;
         }
+        int leftMin = nums[0];
+        TreeMap<Integer, Integer> rightAll = new TreeMap<>();
+        for (int i = 2; i < n; i++) {
+            rightAll.put(nums[i], rightAll.getOrDefault(nums[i], 0) + 1);
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (leftMin < nums[i]) {
+                Integer k = rightAll.ceilingKey(leftMin + 1);
+                if (k != null && k < nums[i]) {
+                    return true;
+                }
+            }
+            leftMin = Math.min(leftMin, nums[i]);
+            rightAll.put(nums[i + 1], rightAll.get(nums[i + 1]) - 1);
+            if (rightAll.get(nums[i + 1]) <= 0) {
+                rightAll.remove(nums[i + 1]);
+            }
+        }
+        return false;
+    }
 
-        @Override
-        public boolean hasNext() {
-            return index < integerList.size();
+    /**
+     * i<j<k && nums[i] < nums[k] < nums[j]
+     * 从后往前遍历
+     * max_k用于维护k能成为的最大值
+     * 使用单调栈(从栈顶到栈底分别从小排到大)
+     * 对于每一个元素
+     * 首先考虑是否能成为i, 若能, 则找到答案
+     * 然后考虑是否能成为j, 不断与栈顶元素比较, 若大于则弹出的元素可能成为k, 最后放入队列
+     * 遍历结束没找到一组数据, 则无解
+     */
+    public static boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return false;
         }
+        Integer max_k = Integer.MIN_VALUE;
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(nums[n-1]);
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < max_k) {
+                return true;
+            }
+            if (!stack.isEmpty() && nums[i] > stack.peek()) {
+                max_k = stack.pop();
+            }
+            if (nums[i] > max_k) {
+                stack.push(nums[i]);
+            }
+        }
+        return false;
     }
 
 
     public static void main(String[] args) {
-
+        System.out.println(find132pattern(new int[]{3,5,0,3,4}));
     }
 }
 
