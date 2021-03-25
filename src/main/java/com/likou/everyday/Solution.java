58abc5b4c5d81683f30df0bf15e12c0f17060075
linqianxu <linqianxu@engintime.com>
2021-03-26 07:53:39 +0800

    83. 删除排序链表中的重复元素

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -8,31 +8,6 @@ public class Solution {
     /**
      * 普普通通删除重复结点
      */
-    public ListNode deleteDuplicates1(ListNode head) {
-        ListNode tempHead = new ListNode();
-        ListNode current = head;
-        ListNode tempCurrent = tempHead;
-        int val = 0;
-        while (current != null) {
-            val = current.val;
-            if (current.next != null && current.next.val == val) {
-                current = current.next;
-                while (current != null && current.val == val) {
-                    current = current.next;
-                }
-            } else {
-                tempCurrent.next = current;
-                current = current.next;
-                tempCurrent = tempCurrent.next;
-                tempCurrent.next = null;
-            }
-        }
-        return tempHead.next;
-    }
-
-    /**
-     * 普普通通删除重复结点
-     */
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null) {
             return head;
