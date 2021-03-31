e53d490491a5a66fa9a5c9645446c11845ba75be
linqianxu <linqianxu@engintime.com>
2021-03-31 18:27:44 +0800

    74. 搜索二维矩阵

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Solution {
 
     /**
-     *
+     * 从右上角开始, target大于当前值则向下走, 小于则向左走
      */
     public boolean searchMatrix(int[][] matrix, int target) {
         int row = matrix.length;
@@ -16,16 +16,18 @@ public class Solution {
         int x = col - 1, y = 0;
         while (true) {
             if (matrix[y][x] < target) {
+                //target小于当前下标的正右结点, 说明target位于两个结点之间, 找不到目标
                 if (x < col - 1 && matrix[y][x+1] > target) {
                     break;
                 }
                 y++;
+                //越界
                 if (y < 0 || y >= row) {
                     break;
                 }
             } else if (matrix[y][x] > target) {
-
                 x--;
+                //越界
                 if (x < 0 || x >= col) {
                     break;
                 }
