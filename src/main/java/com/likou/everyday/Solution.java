8cf7957ce920f5b35e7fb86bb2a084e5ed774a11
linqianxu <linqianxu@engintime.com>
2021-04-05 10:11:02 +0800

    781. 森林中的兔子

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -2,35 +2,39 @@ package com.likou.everyday;
 
 
 
-
 public class Solution {
 
     /**
-     * 动态规划
-     * 注意dp的行列
-     * dp[i][j]代表text1[0...i]和text2[0...j]的最长公共子序列
-     * 当text1[i-1]==text2[j-1]时, text[i][j] = text[i-1][j-1]+1
-     * 当text1[i-1]!=text2[j-1]时, text[i][j] = max(text[i][j-1],text[i-1][j])
+     * 分析可得同一种颜色兔子说的数字n最多只能有n+1次
+     * 当超过n+1时要考虑作为另外一种兔子,因此同一个数字最多有
+     * (Math.ceil((double)frequency[i] / (i + 1)) * (i + 1))
      */
-    public static int longestCommonSubsequence(String text1, String text2) {
-        int length1 = text1.length();
-        int length2 = text2.length();
-        int [][]dp = new int[length1+1][length2+1];
-        for (int i = 1; i <= length1; i++) {
-            char ch = text1.charAt(i-1);
-            for (int j = 1; j <= length2; j++) {
-                if (text2.charAt(j-1) == ch) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
-                }
+    public static int numRabbits(int[] answers) {
+        int length = answers.length;
+        if (length == 0) {
+            return 0;
+        }
+        int result = 0;
+        int[] frequency = new int[1000];
+        for (int answer : answers) {
+            frequency[answer]++;
+        }
+        for (int i = 0; i < 1000; i++) {
+            if (frequency[i] > 0) {
+//                if (frequency[i] >= i + 1) {
+//                    result += (frequency[i] / (i + 1) * (i + 1));
+//                }
+//                if (frequency[i] % (i + 1) != 0) {
+//                    result += i + 1;
+//                }
+                result += Math.ceil((double)frequency[i] / (i + 1)) * (i + 1);
             }
         }
-        return dp[length1][length2];
+        return result;
     }
 
     public static void main(String[] args) {
-        longestCommonSubsequence("abcde", "ace");
+        numRabbits(new int[]{0,0,1,1,1});
     }
 }
 
