2f0697f4a0f598046370393dc7b67de0957b10a0
linqianxu <linqianxu@engintime.com>
2021-04-06 07:30:49 +0800

    80. 删除有序数组中的重复项 II

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -5,37 +5,32 @@ import java.util.Arrays;
 
 public class Solution {
 
-    /**
-     * 两数组从后往前排序即可, 不需要额外的空间
-     */
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (n == 0) {
-            return;
+    public static int removeDuplicates(int[] nums) {
+        int slow = 0, fast = 0;
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
         }
-//        if (m == 0) {
-//            nums1 = nums2;
+//        while (fast < n) {
+//            nums[slow++] = nums[fast++];
+//            while (fast >= 1 && fast < n - 1 && nums[fast] == nums[fast-1] && nums[fast] == nums[fast+1]) {
+//                fast++;
+//            }
 //        }
-        int index = nums1.length - 1, index1 = m - 1, index2 = n - 1;
-        while (index1 >= 0 || index2 >= 0) {
-            if (index1 >= 0 && index2 >= 0) {
-                if (nums1[index1] > nums2[index2]) {
-                    nums1[index--] = nums1[index1--];
-                } else {
-                    nums1[index--] = nums2[index2--];
-                }
-            } else if (index1 >=0) {
-                nums1[index--] = nums1[index1--];
-            } else {
-                nums1[index--] = nums2[index2--];
+        slow = 2;
+        fast = 2;
+        while (fast < n) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                slow++;
             }
+            fast++;
         }
+        return slow;
     }
 
     public static void main(String[] args) {
-        int []nums1 = new int[]{0};
-        int []nums2 = new int[]{1};
-        merge(nums1, 0, nums2, 1);
-        System.out.println(Arrays.toString(nums1));
+        System.out.println(removeDuplicates(new int[]{1, 1, 1, 2, 2, 3}));
     }
 }
 
