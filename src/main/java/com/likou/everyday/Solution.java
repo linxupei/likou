269c86e4998624d91911e32bc505a3a68e22092d
linqianxu <linqianxu@engintime.com>
2021-04-07 07:39:35 +0800

    81. 搜索旋转排序数组 II

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -5,36 +5,36 @@ import java.util.Arrays;
 
 public class Solution {
 
-    /**
-     * 第一钟使用了判断原数组中是否存在三个以上的相同数字思想
-     * 第二种使用了判断更改数组中是否已经存在了两个相同数字思想
-     */
-    public static int removeDuplicates(int[] nums) {
-        int slow = 0, fast = 0;
-        int n = nums.length;
-        if (n <= 2) {
-            return n;
-        }
-//        while (fast < n) {
-//            nums[slow++] = nums[fast++];
-//            while (fast >= 1 && fast < n - 1 && nums[fast] == nums[fast-1] && nums[fast] == nums[fast+1]) {
-//                fast++;
-//            }
-//        }
-        slow = 2;
-        fast = 2;
-        while (fast < n) {
-            if (nums[slow - 2] != nums[fast]) {
-                nums[slow] = nums[fast];
-                slow++;
+    public static boolean search(int[] nums, int target) {
+        int low = 0, height = nums.length - 1;
+        while (low <= height) {
+            int mid = low + ((height - low) >> 1);
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[low] == nums[mid] && nums[mid] == nums[height]) {
+                //遇到类似情况[1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1], 2
+                low++;
+                height--;
+            } else if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target <= nums[mid]) {
+                    height = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[height]) {
+                    low = mid + 1;
+                } else {
+                    height = mid - 1;
+                }
             }
-            fast++;
         }
-        return slow;
+        return false;
     }
 
+
     public static void main(String[] args) {
-        System.out.println(removeDuplicates(new int[]{1, 1, 1, 2, 2, 3}));
+        System.out.println(search(new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1}, 2));
     }
 }
 
