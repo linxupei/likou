9a8d0b52fecc16bc20b5aa925ce9177a111d1c22
linqianxu <linqianxu@engintime.com>
2021-04-09 07:08:05 +0800

    154. 寻找旋转排序数组中的最小值 II

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -5,32 +5,16 @@ import java.util.Arrays;
 
 public class Solution {
 
-    public static int findMin_1(int[] nums) {
-        int low = 0, height = nums.length - 1;
-        while (low < height) {
-            //最小值在该范围, 且范围内数字是递增
-            if (nums[low] <= nums[height]) {
-                return nums[low];
-            }
-            int mid = low + ((height - low) >> 1);
-            //最小值在该范围, 且范围内数字是递减
-            if (nums[low] >= nums[mid] && nums[mid] >= nums[height]) {
-                return nums[height];
-            }
-            if (nums[mid] < nums[height]) {
-                height = mid;
-            } else {
-                low = mid;
-            }
-        }
-        return nums[low];
-    }
 
     public static int findMin(int[] nums) {
         int low = 0, height = nums.length - 1;
         while (low < height) {
             int mid = low + ((height - low) >> 1);
-            if (nums[mid] < nums[height]) {
+            if (nums[mid] == nums[low] && nums[mid] == nums[height]) {
+                //三个位置都相等时无法判断是否还有更小值, 先把左右两个相等的去掉
+                low++;
+                height--;
+            } else if (nums[mid] <= nums[height]) {
                 //不减1, 避免排除最小值
                 height = mid;
             } else {
@@ -42,7 +26,7 @@ public class Solution {
 
 
     public static void main(String[] args) {
-        System.out.println(findMin(new int[]{3,1,2}));
+        System.out.println(findMin(new int[]{2,2,2,2,2,2,2,2,2,2,2,1,2,2}));
     }
 }
 
