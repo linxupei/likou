ab942f904ebcaa8f9af21a508314ab7a8d091f79
linqianxu <linqianxu@engintime.com>
2021-04-13 07:28:16 +0800

    783. 二叉搜索树节点最小距离

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -4,37 +4,43 @@ package com.likou.everyday;
 import lombok.extern.java.Log;
 
 import java.sql.PreparedStatement;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.PriorityQueue;
+import java.util.*;
 import java.util.regex.Matcher;
 
 public class Solution {
 
 
-
     /**
-     * 直接比较两个字符串拼接后的不同值
+     * 中序遍历把所有结点找出, 由于是二叉搜索树, 此时已排好序, 找最小差值
      */
-    public static String largestNumber(int[] nums) {
-        int length = nums.length;
-        String[] strs = new String[length];
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            strs[i] = "" + nums[i];
+    public int minDiffInBST(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        readNode(list, root);
+        int result = Integer.MAX_VALUE;
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            int diff = list.get(i) - list.get(i - 1);
+            if (diff < result) {
+                result = diff;
+            }
         }
-        Arrays.sort(strs, (str1, str2) -> (str2 + str1).compareTo(str1 + str2));
-        for (int i = 0; i < length; i++) {
-            result.append(strs[i]);
+        return result;
+    }
+
+
+
+    public void readNode(List<Integer> list, TreeNode root) {
+        if (root == null) {
+            return;
         }
-        String ans = result.toString();
-        return ans.charAt(0) == '0' ? "0" : ans;
+        readNode(list, root.left);
+        list.add(root.val);
+        readNode(list, root.right);
     }
 
 
     public static void main(String[] args) {
-        System.out.println(largestNumber(new int[]{3636,36}));
+
     }
 }
 
