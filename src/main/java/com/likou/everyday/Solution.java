0d9701b7b83565995f075124970ac1191f4f9997
linqianxu <linqianxu@engintime.com>
2021-04-15 08:34:51 +0800

    213. 打家劫舍 II

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -1,88 +1,50 @@
 package com.likou.everyday;
 
 
-import lombok.extern.java.Log;
-
-import java.sql.PreparedStatement;
 import java.util.*;
-import java.util.regex.Matcher;
 
 public class Solution {
+    /**
+     * 可以仅使用两个变量保存结果
+     *
+     * dp[i]表示小偷经过第i间房子是可以偷到的最小金额
+     * dp[i+1] = Math.max(dp[i], dp[i-1] + nums[i]);
+     * 对于每一间房子的金额可以不偷, 也可以在前一间房子没被偷的情况下偷
+     *
+     */
+    public static int robI(int[] nums) {
+        int length = nums.length;
+        if (length < 1) {
+            return 0;
+        }
+        int[] dp = new int[length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 1; i < length; i++) {
+            dp[i+1] = Math.max(dp[i], dp[i-1] + nums[i]);
+        }
+        return dp[length];
+    }
+
+    /**
+     * 第一间房子和最后一间房子不能同时偷窃
+     * 因此比较偷[0, n-1]和[1,n]的取值即可
+     */
+    public static int rob(int[] nums) {
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        } else if (length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        return Math.max(robI(Arrays.copyOfRange(nums,0,length-1)), robI(Arrays.copyOfRange(nums,1,length)));
+    }
 
     public static void main(String[] args) {
-
+        System.out.println(rob(new int[]{1,3,1,3,100}));
     }
 }
 
 
-class Trie {
 
-    static class TrieNode {
-        public TrieNode[] next;
-        public boolean isLast;
-        public int size;
-        TrieNode() {}
-
-        TrieNode(int size) {
-            this.size = size;
-            next = new TrieNode[size];
-        }
-    }
-
-    private static final int SIZE = 26;
-
-    TrieNode root;
-
-    /** Initialize your data structure here. */
-    public Trie() {
-        root = new TrieNode(SIZE);
-    }
-
-    /** Inserts a word into the trie. */
-    public void insert(String word) {
-        char[] chars = word.toCharArray();
-        int length = chars.length;
-        TrieNode p = root;
-        for (int i = 0; i < length; i++) {
-            char ch = chars[i];
-            TrieNode child = p.next[ch - 'a'];
-            if (child == null) {
-                TrieNode node = new TrieNode(SIZE);
-                p.next[ch - 'a'] = node;
-                p = node;
-            } else {
-                p = child;
-            }
-        }
-        p.isLast = true;
-    }
-
-    /** Returns if the word is in the trie. */
-    public boolean search(String word) {
-        TrieNode node = find(word);
-        return node != null && node.isLast;
-    }
-
-    /** Returns if there is any word in the trie that starts with the given prefix. */
-    public boolean startsWith(String prefix) {
-        TrieNode node = find(prefix);
-        return node != null;
-    }
-
-    public TrieNode find(String word) {
-        char[] chars = word.toCharArray();
-        int length = chars.length;
-        TrieNode p = root;
-        for (int i = 0; i < length; i++) {
-            char ch = chars[i];
-            TrieNode child = p.next[ch - 'a'];
-            if (child == null) {
-                return null;
-            } else {
-                p = child;
-            }
-        }
-        return p;
-    }
-}
 
