b4ceb3aa2908247ca06b5708496e522bf9a03ac4
linqianxu <linqianxu@engintime.com>
2021-04-16 08:52:00 +0800

    87. 扰乱字符串

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -5,43 +5,76 @@ import java.util.*;
 
 public class Solution {
     /**
-     * 可以仅使用两个变量保存结果
-     *
-     * dp[i]表示小偷经过第i间房子是可以偷到的最小金额
-     * dp[i+1] = Math.max(dp[i], dp[i-1] + nums[i]);
-     * 对于每一间房子的金额可以不偷, 也可以在前一间房子没被偷的情况下偷
-     *
+     * 两个字符串不具有扰乱字符串关系的情况
+     * 1.长度不等
+     * 2.字符种类数目不等
+     * 具有扰乱关系
+     * 1.直接相等
+     * 2.1 没有交换位置
+     * 2.2 交换了位置
      */
-    public static int robI(int[] nums) {
-        int length = nums.length;
-        if (length < 1) {
-            return 0;
+    //记忆搜索数组, -1表示false, 1表示true, 0表示未计算
+    int[][][] memo;
+    String s1, s2;
+    public boolean isScramble(String s1, String s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+        int length = s1.length();
+        this.memo = new int[length][length][length + 1];
+        return dfs(0, 0, length);
+    }
+
+    public boolean dfs(int i1, int i2, int length) {
+        if (memo[i1][i2][length] != 0) {
+            return memo[i1][i2][length] == 1;
         }
-        int[] dp = new int[length + 1];
-        dp[0] = 0;
-        dp[1] = nums[0];
+        //判断两个字符串是否相等
+        if (s1.substring(i1, i1 + length).equals(s2.substring(i2, i2 + length))) {
+            memo[i1][i2][length] = 1;
+            return true;
+        }
+        if (!checkIfSimilar(i1, i2, length)) {
+            memo[i1][i2][length] = -1;
+            return false;
+        }
+        //枚举位置
         for (int i = 1; i < length; i++) {
-            dp[i+1] = Math.max(dp[i], dp[i-1] + nums[i]);
+            //没有交换位置
+            if (dfs(i1, i2, i) && dfs(i1 + i, i2 + i, length - i)) {
+                memo[i1][i2][length] = 1;
+                return true;
+            }
+            //换了位置
+            if (dfs(i1, i2 + length - i, i) && dfs(i1 + i, i2, length - i)) {
+                memo[i1][i2][length] = 1;
+                return true;
+            }
         }
-        return dp[length];
+        memo[i1][i2][length] = -1;
+        return false;
     }
 
     /**
-     * 第一间房子和最后一间房子不能同时偷窃
-     * 因此比较偷[0, n-1]和[1,n]的取值即可
+     * 判断两个字符串是否相似
      */
-    public static int rob(int[] nums) {
-        int length = nums.length;
-        if (length == 1) {
-            return nums[0];
-        } else if (length == 2) {
-            return Math.max(nums[0], nums[1]);
+    public boolean checkIfSimilar(int i1, int i2, int length) {
+        int []frequency = new int[26];
+        for (int i = i1; i < i1 + length; i++) {
+            frequency[s1.charAt(i) - 'a']++;
         }
-        return Math.max(robI(Arrays.copyOfRange(nums,0,length-1)), robI(Arrays.copyOfRange(nums,1,length)));
+        for (int i = i2; i < i2 + length; i++) {
+            frequency[s2.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (frequency[i] != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(rob(new int[]{1,3,1,3,100}));
+
     }
 }
 
