7d3a96198906d7daf68eeb6e0efd24846062a3f0
linqianxu <linqianxu@engintime.com>
2021-04-17 19:10:34 +0800

    220. 存在重复元素 III 219. 存在重复元素 II

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -4,79 +4,88 @@ package com.likou.everyday;
 import java.util.*;
 
 public class Solution {
-    /**
-     * 两个字符串不具有扰乱字符串关系的情况
-     * 1.长度不等
-     * 2.字符种类数目不等
-     * 具有扰乱关系
-     * 1.直接相等
-     * 2.1 没有交换位置
-     * 2.2 交换了位置
-     */
-    //记忆搜索数组, -1表示false, 1表示true, 0表示未计算
-    int[][][] memo;
-    String s1, s2;
-    public boolean isScramble(String s1, String s2) {
-        this.s1 = s1;
-        this.s2 = s2;
-        int length = s1.length();
-        this.memo = new int[length][length][length + 1];
-        return dfs(0, 0, length);
-    }
 
-    public boolean dfs(int i1, int i2, int length) {
-        if (memo[i1][i2][length] != 0) {
-            return memo[i1][i2][length] == 1;
-        }
-        //判断两个字符串是否相等
-        if (s1.substring(i1, i1 + length).equals(s2.substring(i2, i2 + length))) {
-            memo[i1][i2][length] = 1;
-            return true;
-        }
-        if (!checkIfSimilar(i1, i2, length)) {
-            memo[i1][i2][length] = -1;
-            return false;
-        }
-        //枚举位置
-        for (int i = 1; i < length; i++) {
-            //没有交换位置
-            if (dfs(i1, i2, i) && dfs(i1 + i, i2 + i, length - i)) {
-                memo[i1][i2][length] = 1;
+    /**
+     * 滑动窗口+TreeSet
+     * abs(nums[i]-nums[j]) <= t, 即nums[j]落在区间[nums[i]-t, nums[i]+t]
+     */
+    public static boolean containsNearbyAlmostDuplicate1(int[] nums, int k, int t) {
+        int length = nums.length;
+        TreeSet<Long> treeSet = new TreeSet<>();
+        for (int i = 0; i < length; i++) {
+            Long x = (long) nums[i];
+            Long y = treeSet.ceiling(x - t);
+            treeSet.add(x);
+            if (y != null && y <= x + t) {
                 return true;
             }
-            //换了位置
-            if (dfs(i1, i2 + length - i, i) && dfs(i1 + i, i2, length - i)) {
-                memo[i1][i2][length] = 1;
-                return true;
+            if (i >= k) {
+                treeSet.remove((long)nums[i-k]);
             }
         }
-        memo[i1][i2][length] = -1;
         return false;
     }
 
     /**
-     * 判断两个字符串是否相似
+     * 滑动窗口+桶排序(Hash表实现)
+     * 设定桶的大小为t+1
+     * nums[i] = (t+1) * a(桶编号) + b(0<b<t)
      */
-    public boolean checkIfSimilar(int i1, int i2, int length) {
-        int []frequency = new int[26];
-        for (int i = i1; i < i1 + length; i++) {
-            frequency[s1.charAt(i) - 'a']++;
-        }
-        for (int i = i2; i < i2 + length; i++) {
-            frequency[s2.charAt(i) - 'a']--;
-        }
-        for (int i = 0; i < 26; i++) {
-            if (frequency[i] != 0) {
-                return false;
+    public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        int length = nums.length;
+        long w = t + 1;
+        HashMap<Long, Long> hashMap = new HashMap<>(k / 3 * 4);
+        for (int i = 0; i < length; i++) {
+            long id = getID(nums[i], w);
+            if (hashMap.containsKey(id)) {
+                return true;
+            }
+            if (hashMap.containsKey(id - 1) && Math.abs(nums[i] - hashMap.get(id - 1)) <= t) {
+                return true;
+            }
+            if (hashMap.containsKey(id + 1) && Math.abs(nums[i] - hashMap.get(id + 1)) <= t) {
+                return true;
+            }
+            hashMap.put(id, (long) nums[i]);
+            if (i >= k) {
+                hashMap.remove(getID(nums[i-k], w));
             }
         }
-        return true;
+        return false;
+    }
+
+    public static long getID(long x, long w) {
+        if (x >= 0) {
+           return x / w;
+        }
+        //正数从0开始分桶
+        //负数从-1开始分桶
+        return (x + 1) / w - 1;
+    }
+
+    /**
+     * 滑动窗口+桶排序
+     **/
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            if (hashSet.contains(nums[i])) {
+                return true;
+            }
+            hashSet.add(nums[i]);
+            if (i >= k) {
+                hashSet.remove(nums[i-k]);
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-
+        containsNearbyAlmostDuplicate(new int[]{1,5,9,1,5,9}, 2, 3);
     }
 }
+
 
 
 
