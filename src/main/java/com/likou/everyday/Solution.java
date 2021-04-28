87aff84bab9e64009fbf246e4900791ed15e7ad8
linqianxu <linqianxu@engintime.com>
2021-04-29 07:08:45 +0800

    633. 平方数之和

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -5,69 +5,37 @@ import java.text.DecimalFormat;
 import java.util.*;
 
 public class Solution {
-
-    public int rangeSumBST(TreeNode root, int low, int high) {
-//        List<Integer> arr = new ArrayList<>();
-//        getNodes(arr, root);
-//        int lowIndex = binarySearch(arr, low);
-//        int heightIndex = binarySearch(arr, high);
-//        int ret = 0;
-//        for (int i = lowIndex; i <= heightIndex; i++) {
-//            ret += arr.get(i);
-//        }
-//        return ret;
-        return getSum(root, low, high);
-    }
-
-
-    public int getSum(TreeNode root, int low, int height) {
-        if (root == null) {
-            return 0;
-        }
-        //当root.val比height还大, 只能到左子树查找, 右子树的每个结点比root.val都大
-        if (root.val > height) {
-            return getSum(root.left, low, height);
-        }
-        //当root.val比low还小, 只能到右子树查找, 左子树的每个结点比root.val都小
-        if (root.val < low) {
-            return getSum(root.right, low, height);
-        }
-        return root.val + getSum(root.left, low, height) + getSum(root.right, low, height);
-    }
-
-    //中序遍历
-    public void getNodes(List<Integer> arr, TreeNode root) {
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        TreeNode p = root;
-        while (p != null || !stack.isEmpty()) {
-            while (p != null) {
-                stack.push(p);
-                p = p.left;
+    /**
+     * 放缩边界
+     */
+    public boolean judgeSquareSum1(int c) {
+        int left = 0, right = (int) Math.sqrt(c);
+        while (left <= right) {
+            int sum = left * left + right * right;
+            if (sum == c) {
+                return true;
             }
-            if (!stack.isEmpty()) {
-                p = stack.pop();
-                arr.add(p.val);
-                p = p.right;
-            }
-        }
-    }
-
-    //二分查找法
-    public int binarySearch(List<Integer> arr, Integer target) {
-        //在arr[l...h]中查找
-        int l = 0, h = arr.size() - 1;
-        while (l <= h) {
-            int mid = l + ((h - l) >> 1);
-            if (arr.get(mid).compareTo(target) == 0) {
-                return mid;
-            }
-            if (arr.get(mid).compareTo(target) > 0) {
-                h = mid - 1;
+            if (sum > c) {
+                right--;
             } else {
-                l = mid + 1;
+                left++;
             }
         }
-        return -1;
+        return false;
+    }
+
+    /**
+     * x*x + y*y = c
+     * 使用sqrt(c - x*x)函数判断是否为整数即可
+     */
+    public boolean judgeSquareSum2(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            double b = Math.sqrt(c - a * a);
+            if (b == (int)b) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
