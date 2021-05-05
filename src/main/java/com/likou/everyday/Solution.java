7a69a2be22da1f442a8c0e4c91ed8d0a699636b9
linqianxu <linqianxu@engintime.com>
2021-05-05 22:57:10 +0800

    740. 删除并获得点数

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -5,62 +5,29 @@ import java.text.DecimalFormat;
 import java.util.*;
 
 public class Solution {
-    // 极大值
-    // 选择 Integer.MAX_VALUE / 2 的原因是防止整数相加溢出
-    static final int INFTY = Integer.MAX_VALUE / 2;
-
-    public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
-        // 将颜色调整为从 0 开始编号，没有被涂色标记为 -1
-        for (int i = 0; i < m; ++i) {
-            --houses[i];
+    /**
+     * 统计每一个数字出现的频率 + 对数字进行排序 + 动态规划
+     * dp[i]代表来到这个数字的时候能获得的最大值
+     * dp[i] =
+     **/
+    public int deleteAndEarn(int[] nums) {
+        int max = Arrays.stream(nums).max().getAsInt();
+        int[] acount = new int[max + 1];
+        for (int i : nums) {
+            acount[i]++;
         }
-
-        // dp 所有元素初始化为极大值
-        int[][][] dp = new int[m][n][target];
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                Arrays.fill(dp[i][j], INFTY);
-            }
+        int[] dp = new int[max + 1];
+        dp[0] = 0;
+        dp[1] = acount[1];
+        for (int i = 2; i <= max; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + acount[i] * i);
         }
-
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (houses[i] != -1 && houses[i] != j) {
-                    continue;
-                }
-
-                for (int k = 0; k < target; ++k) {
-                    for (int j0 = 0; j0 < n; ++j0) {
-                        if (j == j0) {
-                            if (i == 0) {
-                                if (k == 0) {
-                                    dp[i][j][k] = 0;
-                                }
-                            } else {
-                                dp[i][j][k] = Math.min(dp[i][j][k], dp[i - 1][j][k]);
-                            }
-                        } else if (i > 0 && k > 0) {
-                            dp[i][j][k] = Math.min(dp[i][j][k], dp[i - 1][j0][k - 1]);
-                        }
-                    }
-
-                    if (dp[i][j][k] != INFTY && houses[i] == -1) {
-                        dp[i][j][k] += cost[i][j];
-                    }
-                }
-            }
-        }
-
-        int ans = INFTY;
-        for (int j = 0; j < n; ++j) {
-            ans = Math.min(ans, dp[m - 1][j][target - 1]);
-        }
-        return ans == INFTY ? -1 : ans;
+        return dp[max];
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        //System.out.println(solution.getImportance(new int[]{2, 2, 3, 2}));
+        System.out.println(solution.deleteAndEarn(new int[]{2, 2, 3, 2}));
     }
 }
 
