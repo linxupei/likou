f3bad0c751d4961cf909526b896a9c8e517836c7
linqianxu <linqianxu@engintime.com>
2021-05-06 07:55:49 +0800

    1720. 解码异或后的数组

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -6,28 +6,23 @@ import java.util.*;
 
 public class Solution {
     /**
-     * 统计每一个数字出现的频率 + 对数字进行排序 + 动态规划
-     * dp[i]代表来到这个数字的时候能获得的最大值
-     * dp[i] =
-     **/
-    public int deleteAndEarn(int[] nums) {
-        int max = Arrays.stream(nums).max().getAsInt();
-        int[] acount = new int[max + 1];
-        for (int i : nums) {
-            acount[i]++;
+     * X xor X = 0
+     * 0 xor X = X
+     * X xor X xor Y  = Y
+     */
+    public int[] decode(int[] encoded, int first) {
+        int length = encoded.length;
+        int[] arr = new int[length + 1];
+        arr[0] = first;
+        for (int i = 1; i <= length; i++) {
+            arr[i] = arr[i - 1] ^ encoded[i-1];
         }
-        int[] dp = new int[max + 1];
-        dp[0] = 0;
-        dp[1] = acount[1];
-        for (int i = 2; i <= max; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + acount[i] * i);
-        }
-        return dp[max];
+        return arr;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.deleteAndEarn(new int[]{2, 2, 3, 2}));
+        System.out.println(solution.decode(new int[]{2, 2, 3, 2}, 1));
     }
 }
 
