e7a81555d0fc0db933f19b5c579acca6c0c99a64
linqianxu <linqianxu@engintime.com>
2021-05-07 22:53:09 +0800

    1486. 数组异或操作

--- src/main/java/com/likou/everyday/Solution.java
+++ src/main/java/com/likou/everyday/Solution.java
@@ -5,24 +5,18 @@ import java.text.DecimalFormat;
 import java.util.*;
 
 public class Solution {
-    /**
-     * X xor X = 0
-     * 0 xor X = X
-     * X xor X xor Y  = Y
-     */
-    public int[] decode(int[] encoded, int first) {
-        int length = encoded.length;
-        int[] arr = new int[length + 1];
-        arr[0] = first;
-        for (int i = 1; i <= length; i++) {
-            arr[i] = arr[i - 1] ^ encoded[i-1];
+
+    public int xorOperation(int n, int start) {
+        int result = start;
+        for (int i = 1; i < n; i++) {
+            result ^= (start + 2 * i);
         }
-        return arr;
+        return result;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.decode(new int[]{2, 2, 3, 2}, 1));
+        System.out.println(solution.xorOperation(5, 1));
     }
 }
 
